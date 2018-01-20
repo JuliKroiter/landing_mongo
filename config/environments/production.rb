cc38791cb86d99824bfda00ac028b183d1f01ef4
JuliKroiter <kroiter09@gmail.com>
2018-01-20 21:33:18 +0200

    Email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,17 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'localhost:3000',
+    user_name: Rails.application.secrets.gmail_email,
+    password: Rails.application.secrets.gmail_pass,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
