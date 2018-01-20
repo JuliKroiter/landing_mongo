222c70c468013981d59e0fcc71c208b86e71a27f
JuliKroiter <kroiter09@gmail.com>
2018-01-20 21:47:00 +0200

    Production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'whispering-bastion-89780.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'localhost:3000',
+    domain: 'whispering-bastion-89780.herokuapp.com',
     user_name: Rails.application.secrets.gmail_email,
     password: Rails.application.secrets.gmail_pass,
     authentication: :plain,
